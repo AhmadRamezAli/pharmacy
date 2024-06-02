0a1d9c74ce437ef3cfed25ba8429b1bf180d484d
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:34:09 +0300

    di config

---
+++ Pharmacy.Infrastracture/DI/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pharmacy.Domain.Repositories;
+using Pharmacy.Infrastracture.DataContext;
+using Pharmacy.Infrastracture.Repositories;
+using Pharmacy.Infrastracture.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Infrastracture.DI;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, string conncetionString)
+    {
+        services.AddAutoMapper(typeof(MapperProfile).Assembly);
+        services.AddDbContext<PharmacyContext>(options =>
+        {
+            options.UseSqlServer(conncetionString);
+        });
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+}
