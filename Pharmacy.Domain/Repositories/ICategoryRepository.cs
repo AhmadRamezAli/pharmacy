f6346635850e36c348d38e533df89c04ebea09ab
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:33:24 +0300

    domain repos

---
+++ Pharmacy.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Pharmacy.Domain.Entities;
+using Pharmacy.SharedKernel.Repository;
+
+namespace Pharmacy.Domain.Repositories;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    public List<Medicine> GetMedicines(int categoryId);
+}
