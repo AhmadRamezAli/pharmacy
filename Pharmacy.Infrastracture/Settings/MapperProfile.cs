5c583b985de3d0061a9cabaceacceb9077a63557
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:35:24 +0300

    automapper profile

---
+++ Pharmacy.Infrastracture/Settings/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Infrastracture.Models;
+
+namespace Pharmacy.Infrastracture.Settings
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Category, CategoryDAO>();
+            CreateMap<CategoryDAO,Category>();
+        }
+    }
+}
