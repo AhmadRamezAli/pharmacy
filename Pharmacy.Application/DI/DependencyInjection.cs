66c5b207b15ec1d3f30ce453509dda8b49471a62
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:31:46 +0300

    di config

---
+++ Pharmacy.Application/DI/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pharmacy.Application.Services.Category;
+
+namespace Pharmacy.Application.DI;
+
+public static class DependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+    }
+}
