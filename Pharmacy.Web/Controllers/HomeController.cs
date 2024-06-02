1b84b0ab62f62dd9015a7f8ed65c2099bccf188b
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:33:07 +0300

    new web mvc

---
+++ Pharmacy.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Domain.Repositories;
+using Pharmacy.Web.Models;
+using System.Diagnostics;
+
+namespace Pharmacy.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public HomeController(ILogger<HomeController> logger, ICategoryRepository categoryRepository)
+        {
+            _logger = logger;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok(_categoryRepository.GetAll());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Pharmacy.Web/Program.cs
@@ -0,0 +1,35 @@
+using Pharmacy.Domain.Repositories;
+using Pharmacy.Infrastracture.DI;
+using Pharmacy.Infrastracture.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var conncetionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddInfrastructure(conncetionString);
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
