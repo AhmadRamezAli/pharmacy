b7ccec583464e8a43113e278903e983adf007638
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:47:08 +0300

    Update Company

--- Pharmacy.Infrastracture/Models/CompanyDAO.cs
+++ Pharmacy.Infrastracture/Models/CompanyDAO.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 
 namespace Pharmacy.Infrastracture.Models
 {
-    public partial class CompanyDAO
+    public partial class CompanyDAO :DAO
     {
         public CompanyDAO()
         {
             Medicines = new HashSet<MedicineDAO>();
         }
 
-        public int Id { get; set; }
         public string Name { get; set; } = null!;
 
         public virtual ICollection<MedicineDAO> Medicines { get; set; }
