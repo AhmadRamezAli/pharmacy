210729a04e7f17309befae15077a7911a5dc5864
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-02 23:35:45 +0300

    adding repos implementation

---
+++ Pharmacy.Infrastracture/Repositories/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Pharmacy.Infrastracture.DataContext;
+using Pharmacy.Infrastracture.Models;
+using Pharmacy.SharedKernel.DTO;
+using Pharmacy.SharedKernel.Entities;
+using Pharmacy.SharedKernel.Repository;
+
+namespace Pharmacy.Infrastracture.Repositories;
+
+public class BaseRepository<TEntity, TDAO> : IRepository<TEntity>
+    where TEntity : Entity
+    where TDAO : DAO
+{
+
+    protected readonly PharmacyContext _context;
+    protected readonly IMapper _mapper;
+
+    public BaseRepository(PharmacyContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public TEntity Add(TEntity entity)
+    {
+        TDAO dao = _mapper.Map<TDAO>(entity);
+        _context.Set<TDAO>().Add(dao);
+        entity.Id = dao.Id;
+        return entity;
+    }
+
+    public void Delete(TEntity entity)
+    {
+        TDAO dao = _mapper.Map<TDAO>(entity);
+        _context.Set<TDAO>().Remove(dao);
+    }
+
+    public TEntity Get(int id)
+    {
+        TDAO? res = _context.Set<TDAO>().FirstOrDefault(e => e.Id == id);
+        return _mapper.Map<TEntity>(res);
+    }
+
+    public List<TEntity> GetAll()
+    {
+        return _context.Set<TDAO>().Select(e => _mapper.Map<TEntity>(e)).ToList();
+    }
+
+    public PaginatedResponse<TEntity> GetPage(PaginatedRequest request)
+    {
+        return new PaginatedResponse<TEntity>()
+        {
+            Count = _context.Set<TDAO>().Count(),
+            PayLoad = _context.Set<TDAO>()
+                    .OrderBy(e=>e.Id)
+                    .Skip(request.PageSize*(request.PageNumber - 1))
+                    .Take(request.PageSize)
+                    .Select(e => _mapper.Map<TEntity>(e))
+                    .ToList()
+        };
+    }
+
+    #region Update
+    public void Update(TEntity entity)
+    {
+         TDAO dao = _mapper.Map<TDAO>(entity);
+        _context.Set<TDAO>().Update(dao);
+        entity.Id = dao.Id;
+    }
+    #endregion
+}
---
+++ Pharmacy.Infrastracture/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Domain.Repositories;
+using Pharmacy.Infrastracture.DataContext;
+using Pharmacy.Infrastracture.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Infrastracture.Repositories;
+
+public class CategoryRepository : BaseRepository<Category, CategoryDAO>, ICategoryRepository
+{
+    public CategoryRepository(PharmacyContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public List<Medicine> GetMedicines(int categoryId)
+    {
+        return _context
+            .Medicines
+            .Where(e => e.Category == categoryId)
+            .Select(e=>_mapper.Map<Medicine>(e))
+            .ToList();
+    }
+}
