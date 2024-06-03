bf13f945d5007b4168a2f3be15a269f40e80a9ac
aboud426 <abdullahabod170@gmail.com>
2024-06-03 17:33:00 +0300

    DI Config For Application

--- Pharmacy.Application/DI/DependencyInjection.cs
+++ Pharmacy.Application/DI/DependencyInjection.cs
@@ -1,5 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Pharmacy.Application.Services.Category;
+using Pharmacy.Application.Services.Company;
+using Pharmacy.Application.Services.Disease;
+using Pharmacy.Application.Services.DiseaseMedicine;
+using Pharmacy.Application.Services.Ingredient;
+using Pharmacy.Application.Services.Medicien;
+using Pharmacy.Application.Services.MedicienIngredient;
+using Pharmacy.Application.Services.Patient;
+using Pharmacy.Application.Services.PatientDisease;
 
 namespace Pharmacy.Application.DI;
 
@@ -8,5 +16,21 @@ public static class DependencyInjection
     public static void AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ICategoryService, CategoryService>();
+
+        services.AddScoped<ICompanyService, CompanyService>();
+
+        services.AddScoped<IDiseaseService, DiseaseService>();
+
+        services.AddScoped<IDiseaseMedicineService, DiseaseMedicineService>();
+
+        services.AddScoped<IIngredientService, IngredientService>();
+
+        services.AddScoped<IMedicienService, MedicienService>();
+
+        services.AddScoped<IMedicienIngredientService, MedicienIngredientService>();
+
+        services.AddScoped<IPatientService, PatientService>();
+
+        services.AddScoped<IPatientDiseaseService, PatientDiseaseService>();
     }
 }
