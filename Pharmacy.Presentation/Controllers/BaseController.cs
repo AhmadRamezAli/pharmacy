42869474dfa6185c8fc723c275d530287288eb42
aboud426 <abdullahabod170@gmail.com>
2024-06-03 18:55:01 +0300

    Adding Base Controller

---
+++ Pharmacy.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Application.Services;
+using Pharmacy.SharedKernel.DTO;
+using Pharmacy.SharedKernel.Entities;
+
+namespace Pharmacy.Presentation.Controllers;
+
+public class BaseController<TEntity, TCreateRequest,TUpdateRequest> : Controller
+    where TEntity : Entity 
+    where TCreateRequest :  ICreateRequest
+    where TUpdateRequest : IUpdateRequest
+{
+    private readonly BaseService<TEntity> _service;
+    private readonly IMapper _mapper;
+
+    public BaseController(BaseService<TEntity> service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+
+    public IActionResult Index()
+    {
+        var entities = _service.GetAll();
+        return View(entities);
+    }
+
+    public IActionResult Add(TCreateRequest request)
+    {
+        var entity = _mapper.Map<TEntity>(request);
+        _service.Add(entity);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(TUpdateRequest request)
+    {
+        var entity = _mapper.Map<TEntity>(request);
+        _service.Update(entity);
+        return RedirectToAction("Index");
+    }
+    public IActionResult Delete(int id)
+    {
+        var entity = _service.Get(id);
+        _service.Delete(entity);
+        return View();
+    }
+
+}
