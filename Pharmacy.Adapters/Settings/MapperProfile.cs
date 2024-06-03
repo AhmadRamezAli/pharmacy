32458ade6d6f2ee1b0f915e75881419e76ec58ca
aboud426 <abdullahabod170@gmail.com>
2024-06-03 18:53:06 +0300

    Adding Adapters

---
+++ Pharmacy.Adapters/Settings/MapperProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Infrastracture.Models;
+
+namespace Pharmacy.Adapters.Settings;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Category, CategoryDAO>();
+        CreateMap<CategoryDAO, Category>();
+
+        CreateMap<Company, CompanyDAO>();
+        CreateMap<CompanyDAO, Company>();
+
+        CreateMap<Disease, DiseaseDAO>();
+        CreateMap<DiseaseDAO, Disease>();
+
+        CreateMap<DiseaseMedicine, DiseaseMedicineDAO>();
+        CreateMap<DiseaseMedicineDAO, DiseaseMedicine>();
+
+        CreateMap<Ingredient, IngredientDAO>();
+        CreateMap<IngredientDAO, Ingredient>();
+
+        CreateMap<MedicienIngredient, MedicienIngredientDAO>();
+        CreateMap<MedicienIngredientDAO, MedicienIngredient>();
+
+        CreateMap<Medicine, MedicineDAO>();
+        CreateMap<MedicineDAO, Medicine>();
+
+        CreateMap<Patient, PatientDAO>();
+        CreateMap<PatientDAO, Patient>();
+
+        CreateMap<PatientDisease, PatientDiseaseDAO>();
+        CreateMap<PatientDiseaseDAO, PatientDisease>();
+    }
+}
