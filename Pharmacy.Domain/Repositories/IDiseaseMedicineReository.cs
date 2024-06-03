939dd772d4ef0764738ae7b346cd4cc5560e6a02
aboud426 <abdullahabod170@gmail.com>
2024-06-03 04:17:47 +0300

    adding domain repos

---
+++ Pharmacy.Domain/Repositories/IDiseaseMedicineReository.cs
@@ -0,0 +1,14 @@
+﻿using Pharmacy.Domain.Entities;
+using Pharmacy.SharedKernel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Domain.Repositories
+{
+    internal interface IDiseaseMedicineReository : IRepository<DiseaseMedicine>
+    {
+    }
+}
