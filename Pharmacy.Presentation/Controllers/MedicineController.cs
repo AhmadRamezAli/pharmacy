0c285a10ada83207ada3031ccdaf18c66a671a1b
AhmadRamezAli <ahmad.70sahar@gmail.com>
2024-06-04 01:32:38 +0300

    add controller

---
+++ Pharmacy.Presentation/Controllers/MedicineController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Pharmacy.Application.Services;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Presentation.Models.Medicine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Presentation.Controllers
+{
+    public class MedicineController : BaseController<Medicine, CreateMedicineRequest, UpdateMedicineRequest>
+    {
+        public MedicineController(BaseService<Medicine> service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
