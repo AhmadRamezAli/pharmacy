1eab1c784d4c7d28c66ce24cd2fc0f9fe06cb630
aboud426 <abdullahabod170@gmail.com>
2024-06-03 17:34:46 +0300

    mapper profile settings

--- Pharmacy.Infrastracture/Settings/MapperProfile.cs
+++ Pharmacy.Infrastracture/Settings/MapperProfile.cs
@@ -10,6 +10,30 @@ namespace Pharmacy.Infrastracture.Settings
         {
             CreateMap<Category, CategoryDAO>();
             CreateMap<CategoryDAO,Category>();
+
+            CreateMap<Company, CompanyDAO>();
+            CreateMap<CompanyDAO, Company>();
+
+            CreateMap<Disease, DiseaseDAO>();
+            CreateMap<DiseaseDAO, Disease>();
+
+            CreateMap<DiseaseMedicine, DiseaseMedicineDAO>();
+            CreateMap<DiseaseMedicineDAO, DiseaseMedicine>();
+
+            CreateMap<Ingredient, IngredientDAO>();
+            CreateMap<IngredientDAO, Ingredient>();
+
+            CreateMap<MedicienIngredient, MedicienIngredientDAO>();
+            CreateMap<MedicienIngredientDAO, MedicienIngredient>();
+
+            CreateMap<Medicine, MedicineDAO>();
+            CreateMap<MedicineDAO, Medicine>();
+
+            CreateMap<Patient, PatientDAO>();
+            CreateMap<PatientDAO, Patient>();
+
+            CreateMap<PatientDisease, PatientDiseaseDAO>();
+            CreateMap<PatientDiseaseDAO, PatientDisease>();
         }
     }
 }
